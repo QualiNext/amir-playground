b2ff40cb2da876a0061a9b5adde4966693166c76
Amir Rashkovsky <62113999+amiros89@users.noreply.github.com>
2022-10-11 16:59:38 +0300

    Update config.tf

--- assets/envs/dev-cluster/config.tf
+++ assets/envs/dev-cluster/config.tf
@@ -1,12 +1,12 @@
-terraform {
-  backend "s3" {
-    bucket  = "quali-devops-tfstate"
-    region  = "eu-west-1"
-    profile = "devops"
-    key     = "k8s/amir-testing/terraform.tfstate"
-  }
+# terraform {
+#   backend "s3" {
+#     bucket  = "quali-devops-tfstate"
+#     region  = "eu-west-1"
+#     profile = "devops"
+#     key     = "k8s/amir-testing/terraform.tfstate"
+#   }
 
-}
+# }
 provider "aws" {
   region = "eu-west-1"
 }
