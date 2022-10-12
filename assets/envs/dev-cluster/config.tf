69c74c2a69cc314100a82e17f99725e7c09f95ec
amiros89 <62113999+amiros89@users.noreply.github.com>
2022-10-12 09:03:02 +0300

    clean config

--- assets/envs/dev-cluster/config.tf
+++ assets/envs/dev-cluster/config.tf
@@ -1,20 +1,5 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "quali-devops-tfstate"
-#     region  = "eu-west-1"
-#     profile = "devops"
-#     key     = "k8s/amir-testing/terraform.tfstate"
-#   }
-
-# }
 provider "aws" {
   region = "eu-west-1"
-  # assume_role {
-  #   # role_arn     = "arn:aws:iam::123456789012:role/ROLE_NAME"
-  #   role_arn = var.role_arn
-  #   session_name = "SESSION_NAME"
-  #   external_id  = "EXTERNAL_ID"
-  # }
 }
 
 provider "helm" {
